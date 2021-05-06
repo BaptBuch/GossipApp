c513102e373fb26b3c84bd942f61c6b207e2f490
Bapt <baptiste.buch@gmail.com>
2021-05-06 23:24:12 +0200

    first commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,13 +3,12 @@ require 'faker'
 10.times do
   City.create(name: Faker::Address.city, zip_code: Faker::Address.zip_code)
 end
-cities_list = City.all
+
 
 10.times do
   cities_list = City.all
   User.create!(first_name: Faker::Superhero.prefix, last_name: Faker::Superhero.suffix, description: Faker::Hipster.sentence, email:Faker::Internet.email, age: Faker::Number.between(from: 18, to: 100), city: cities_list.sample)
 end
-users = User.all
 
 10.times do
   Tag.create(title: Faker::Superhero.prefix)
