3beda645095e738dfec524d853522b439e5889f9
Bapt <baptiste.buch@gmail.com>
2021-05-06 21:27:41 +0200

    first commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,16 +8,23 @@
 
 require 'faker'
 
+# i = 0
 10.times do
-  User.create!(first_name: Faker::Superhero.prefix, last_name: Faker::Superhero.suffix, description: Faker::Hipster.sentence, email:Faker::Internet.email, age: Faker::Number.between(from: 18, to: 100))
+  City.create(name: Faker::Address.city, zip_code: Faker::Address.zip_code)
+  # users[i].city = cities_list[rand(0..9)]
+  # users[i].save
+  # i +=1
+end
+cities_list = City.all
+
+10.times do
+  cities_list = City.all
+  User.create!(first_name: Faker::Superhero.prefix, last_name: Faker::Superhero.suffix, description: Faker::Hipster.sentence, email:Faker::Internet.email, age: Faker::Number.between(from: 18, to: 100), city: cities_list.sample)
 end
 users = User.all
 
-i = 0
-10.times do
-  City.create(name: Faker::Address.city, zip_code: Faker::Address.zip_code)
-  cities_list = City.all
-  users[i].city = cities_list[rand(0..9)]
-  users[i].save
-  i +=1
+
+20.times do
+  u = User.all.sample
+  Gossip.create(title: Faker::Movie.quote, content: Faker::Lorem.paragraph, user: u)
 end
